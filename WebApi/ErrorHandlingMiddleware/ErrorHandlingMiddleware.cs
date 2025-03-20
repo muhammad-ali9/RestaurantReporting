39e1f88b8996ee64e909f46ab89d3cdb863d60b3
MuhammadAli <USER@DESKTOP-B90QAPU>
2025-03-20 17:19:54 +0100

    Generic Exception Handling Middleware

---
+++ WebApi/ErrorHandlingMiddleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+using System.Text.Json;
+using Application.Exceptions;
+using Application.Response;
+
+namespace WebApi.ErrorHandlingMiddleware
+{
+    public class ErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public ErrorHandlingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                var response = context.Response;
+                response.ContentType = "application/json";
+                var responseModel = new ApiResponse<string> { Message = ex.Message };
+                var result = JsonSerializer.Serialize(responseModel);
+                await response.WriteAsync(result);
+            }
+        }
+    }
+}
