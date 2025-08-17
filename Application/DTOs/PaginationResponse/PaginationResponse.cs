918eb093e8abc7f7512e7a7ec3ec6846576d4d3a
Muhammad Ali <m.a.bangash99@gmail.com>
2025-08-17 13:17:58 +0200

    Adding Generic Pagination

---
+++ Application/DTOs/PaginationResponse/PaginationResponse.cs
@@ -0,0 +1,29 @@
+﻿namespace Application.DTOs.PaginationResponse
+{
+    public class PaginationResponse<T> : List<T>
+    {
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public int PageSize { get; set; }
+        public int TotalCount { get; set; }
+        public bool HasProevious => CurrentPage > 1;
+        public bool HasNext => CurrentPage < TotalPages;
+
+
+        public PaginationResponse(List<T> items, int count, int pageNumber, int pageSize)
+        {
+            CurrentPage = pageNumber;
+            TotalCount = count;
+            PageSize = pageSize;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+            AddRange(items);
+        }
+
+        public static PaginationResponse<T> ToPagedList(IQueryable<T> source, int pageNumber, int pageSize)
+        {
+            var count = source.Count();
+            var items = source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList();
+            return new PaginationResponse<T>(items, count, pageNumber, pageSize);
+        }
+    }
+}
